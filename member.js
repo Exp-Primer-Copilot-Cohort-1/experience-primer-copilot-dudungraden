b04fd44f2560dfbb042282c804ff12fef4f9942c
DUDUNG RADEN <150446398+dudungraden@users.noreply.github.com>
2023-11-26 07:48:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+    var skills = [
+        { name: "HTML", level: "expert" },
+        { name: "CSS", level: "expert" },
+        { name: "JavaScript", level: "expert" },
+        { name: "PHP", level: "expert" },
+        { name: "MySQL", level: "expert" },
+        { name: "Python", level: "intermediate" },
+        { name: "C#", level: "intermediate" },
+        { name: "C++", level: "intermediate" },
+        { name: "Java", level: "intermediate" },
+        { name: "Ruby", level: "intermediate" },
+        { name: "C", level: "intermediate" },
+        { name: "Objective-C", level: "intermediate" },
+        { name: "Swift", level: "intermediate" },
+        { name: "Perl", level: "beginner" },
+        { name: "Visual Basic", level: "beginner" },
+        { name: "R", level: "beginner" },
+        { name: "Matlab", level: "beginner" },
+        { name: "Shell Scripting", level: "beginner" },
+        { name: "Assembly", level: "beginner" }
+    ];
+    return skills;
+}
